4e523250b15d1ce9238fc6abb6395262e167bd55
Joshua Soong <joshua@soong.nz>
2023-08-18 18:17:39 +1200

    adding pagefind

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import pagefind from 'astro-pagefind'
 
 export default defineConfig({
   site: 'https://biochromatic.ink',
-  integrations: [mdx(), sitemap()],
+  build: {
+    format: "file",
+  },
+  integrations: [mdx(), sitemap(), pagefind()],
 })
