740aafa57379c0b710eb795b8949165ad2a483cb
NeedsAdjustment <joshua@soong.nz>
2023-08-18 14:07:00 +1200

    added nojekyll file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://biochromatic.ink',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://biochromatic.ink',
+  integrations: [mdx(), sitemap()],
+})
+
