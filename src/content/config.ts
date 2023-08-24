5cc9d0e89fa69d36d86fa44de479a64e015ae7d2
Joshua Soong <joshua@soong.nz>
2023-08-24 19:24:39 +1200

    added short/long delineation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({
+const long = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -18,4 +18,22 @@ const blog = defineCollection({
   }),
 })
 
-export const collections = { blog }
+const short = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+})
+
+export const collections = { long, short }
